629baa6533794f6f21378ba8bb223f48c5880668
Gustavo Duarte <donhaduarte@gmail.com>
2024-02-03 22:50:23 -0300

    atualização imagem e .js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,24 +1,28 @@
 alert('Boas Vindas ao jogo do número secreto');
-let numeroSecreto = 29;
+let numeroSecreto = 7;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
-if(numeroSecreto == chute){
-    console.log('Isso aí, você descobriu o número secreto');
+
+while (numeroSecreto != chute) {
+    chute = prompt("Digite um número:"); // Solicita um novo chute
+
+    if (numeroSecreto == chute) {
+        alert(`Isso aí, você descobriu o número secreto: ${numeroSecreto}`);
+    } else {
+        alert(`Você errou, não é o número ${chute}`);
+    }
 }
 
+// Explicação da diferença entre concatenação de strings e template literals:
+/*
+Aqui nós temos dois exemplos de criação de strings em JavaScript:
 
-alert('Boas vindas ao nosso site');
-let nome = "lua";
-let idade = 25;
-let numero_de_vendas = 50;
-let saldoDisponivel = 1000;
-alert('Erro! Preencha todos os campos');
-let mensagemErro = "Erro! Preencha todos os campos";
-alert(mensagemErro);
-let nome_ = prompt("Qual seu nome?")
-let idade_ = prompt("Qual sua idade?")
-if(idade_ >= 18){
-    alert('Parabéns você pode entrar')
-}else{
-    alert('De menor né?, engana ninguém pivete')
-}
+1. Concatenação de strings com o operador '+':
+   alert('O número secreto era ' + numeroSecreto + ', mas você escolheu ' + chute);
+   Neste exemplo, estamos concatenando as strings usando o operador '+'. Isso requer a utilização de múltiplos operadores '+', o que pode tornar o código menos legível, especialmente com várias variáveis.
+
+2. Template literals com crases (`):
+   alert(`O número secreto era ${numeroSecreto}, mas você escolheu ${chute}`);
+   Aqui, estamos usando template literals para criar a string. Isso nos permite incluir variáveis dentro da string de forma mais limpa e legível, usando '${...}' para interpolação. Isso torna o código mais fácil de entender e modificar.
+*/
