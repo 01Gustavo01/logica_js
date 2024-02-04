3a68215830d10bd212f4eddff4bd8e2157259571
Gustavo Duarte <donhaduarte@gmail.com>
2024-02-03 22:01:47 -0300

    Responsivo

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,24 @@
+alert('Boas Vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if(numeroSecreto == chute){
+    console.log('Isso aí, você descobriu o número secreto');
+}
+
+
+alert('Boas vindas ao nosso site');
+let nome = "lua";
+let idade = 25;
+let numero_de_vendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemErro = "Erro! Preencha todos os campos";
+alert(mensagemErro);
+let nome_ = prompt("Qual seu nome?")
+let idade_ = prompt("Qual sua idade?")
+if(idade_ >= 18){
+    alert('Parabéns você pode entrar')
+}else{
+    alert('De menor né?, engana ninguém pivete')
+}
